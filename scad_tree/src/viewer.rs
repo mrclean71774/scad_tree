736a6b4f17203dfcb239778f498ed758acd3b145
Michael Phillips <mrclean71774@gmail.com>
2023-03-29 15:00:27 -0400

    Add some more methods to Viewer

--- scad_tree/src/viewer.rs
+++ scad_tree/src/viewer.rs
@@ -23,8 +23,9 @@
 
 use scad_tree_math::Mt4;
 
-use crate::{prelude::*, CubicBezier2D};
+use crate::prelude::*;
 
+/// Viewer struct is used to view points, edges, and curves in OpenSCAD.
 pub struct Viewer {
     point_radius: f64,
     edge_radius: f64,
@@ -42,7 +43,20 @@ impl Viewer {
         }
     }
 
-    pub fn add_point(&mut self, point: Pt3, color: ScadColor) {
+    pub fn add_pt2(&mut self, point: Pt2, color: ScadColor) {
+        let s = translate!([point.x, point.y, 0.0],
+            color!(c=color,
+                sphere!(self.point_radius, fn=self.segments);
+            );
+        );
+        if let Some(scad) = &mut self.scad {
+            self.scad = Some(scad.clone() + s);
+        } else {
+            self.scad = Some(s);
+        }
+    }
+
+    pub fn add_pt3(&mut self, point: Pt3, color: ScadColor) {
         let s = translate!([point.x, point.y, point.z],
             color!(c=color,
                 sphere!(self.point_radius, fn=self.segments);
@@ -85,7 +99,71 @@ impl Viewer {
         }
     }
 
-    pub fn add_edges(&mut self, edges: &Vec<(Pt3, Pt3)>, color: ScadColor) {
+    pub fn add_pt3s(&mut self, points: &Pt3s, color: ScadColor) {
+        let mut children = Vec::with_capacity(points.len());
+        for point in points.iter() {
+            let s = translate!([point.x, point.y, point.z],
+                sphere!(self.point_radius, fn=self.segments);
+            );
+            children.push(s);
+        }
+        let child = Scad {
+            op: ScadOp::Color {
+                rgba: None,
+                color: Some(color),
+                hex: None,
+                alpha: Some(1.0),
+            },
+            children,
+        };
+        if let Some(scad) = &mut self.scad {
+            self.scad = Some(Scad {
+                op: ScadOp::Union,
+                children: vec![scad.clone(), child],
+            });
+        } else {
+            self.scad = Some(Scad {
+                op: ScadOp::Union,
+                children: vec![child],
+            });
+        }
+    }
+
+    pub fn add_edges2d(&mut self, edges: &Vec<(Pt2, Pt2)>, color: ScadColor) {
+        let mut children = Vec::new();
+        for (start, end) in edges {
+            let matrix =
+                Mt4::look_at_matrix_lh(start.as_pt3(0.0), end.as_pt3(0.0), Pt3::new(0.0, 0.0, 1.0));
+            let mut c =
+                Polyhedron::cylinder(self.edge_radius, (*end - *start).len(), self.segments);
+            c.apply_matrix(&matrix);
+            c.translate(start.as_pt3(0.0));
+
+            children.push(polyhedron!(c.points, c.faces));
+        }
+        let child = Scad {
+            op: ScadOp::Color {
+                rgba: None,
+                color: Some(color),
+                hex: None,
+                alpha: Some(1.0),
+            },
+            children,
+        };
+        if let Some(scad) = &mut self.scad {
+            self.scad = Some(Scad {
+                op: ScadOp::Union,
+                children: vec![scad.clone(), child],
+            });
+        } else {
+            self.scad = Some(Scad {
+                op: ScadOp::Union,
+                children: vec![child],
+            });
+        }
+    }
+
+    pub fn add_edges3d(&mut self, edges: &Vec<(Pt3, Pt3)>, color: ScadColor) {
         let mut children = Vec::new();
         for (start, end) in edges {
             let matrix = Mt4::look_at_matrix_lh(*start, *end, Pt3::new(0.0, 0.0, 1.0));
@@ -125,19 +203,35 @@ impl Viewer {
         let edge_count = points.len() - 1;
         let mut edges = Vec::with_capacity(edge_count + 2); // 2 extra for handles
         for i in 0..points.len() - 1 {
-            edges.push((points[i].as_pt3(0.0), points[i + 1].as_pt3(0.0)));
+            edges.push((points[i], points[i + 1]));
         }
 
-        self.add_edges(&edges, ScadColor::White);
-        self.add_edges(
-            &vec![
-                (curve.start.as_pt3(0.0), curve.control1.as_pt3(0.0)),
-                (curve.end.as_pt3(0.0), curve.control2.as_pt3(0.0)),
-            ],
+        self.add_edges2d(&edges, ScadColor::White);
+        self.add_edges2d(
+            &vec![(curve.start, curve.control1), (curve.end, curve.control2)],
             ScadColor::Green,
         );
-        self.add_point(curve.control1.as_pt3(0.0), ScadColor::Green);
-        self.add_point(curve.control2.as_pt3(0.0), ScadColor::Green);
+        self.add_pt2(curve.control1, ScadColor::Green);
+        self.add_pt2(curve.control2, ScadColor::Green);
+    }
+
+    pub fn add_cubic_bezier3d(&mut self, curve: &CubicBezier3D) {
+        let points = curve.gen_points();
+        self.add_pt3s(&points, ScadColor::DarkSlateGray);
+
+        let edge_count = points.len() - 1;
+        let mut edges = Vec::with_capacity(edge_count + 2); // 2 extra for handles
+        for i in 0..points.len() - 1 {
+            edges.push((points[i], points[i + 1]));
+        }
+
+        self.add_edges3d(&edges, ScadColor::White);
+        self.add_edges3d(
+            &vec![(curve.start, curve.control1), (curve.end, curve.control2)],
+            ScadColor::Green,
+        );
+        self.add_pt3(curve.control1, ScadColor::Green);
+        self.add_pt3(curve.control2, ScadColor::Green);
     }
 
     pub fn into_scad(self) -> Scad {
