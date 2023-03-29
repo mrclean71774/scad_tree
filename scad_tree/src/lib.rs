27386f4ea0f805a8c6f0691aaf02d1340c2ce595
Michael Phillips <mrclean71774@gmail.com>
2023-03-29 14:59:43 -0400

    Add curves to prelude

--- scad_tree/src/lib.rs
+++ scad_tree/src/lib.rs
@@ -34,16 +34,18 @@ pub mod prelude {
             circle, color, cube, cylinder, difference, dim2, dim3, fat_thread, hull, import,
             intersection, linear_extrude, metric_thread, minkowski, mirror, polygon, polyhedron,
             projection, resize, rotate, rotate_extrude, scad_file, scale, sphere, square, surface,
-            text, translate, union, Faces, Indices, Paths, Polyhedron, Pt2, Pt2s, Pt3, Pt3s, Pt4,
-            Scad, ScadColor, ScadOp, TextDirection, TextHalign, TextParams, TextValign, Viewer,
+            text, translate, union, CubicBezier2D, CubicBezier3D, CubicBezierChain2D,
+            CubicBezierChain3D, Faces, Indices, Paths, Polyhedron, Pt2, Pt2s, Pt3, Pt3s, Pt4,
+            QuadraticBezier2D, QuadraticBezier3D, Scad, ScadColor, ScadOp, TextDirection,
+            TextHalign, TextParams, TextValign, Viewer,
         },
         std::io::Write,
     };
 }
 
 pub use {
-    dim2::{CubicBezier2D, QuadraticBezier2D},
-    dim3::Polyhedron,
+    dim2::{CubicBezier2D, CubicBezierChain2D, QuadraticBezier2D},
+    dim3::{CubicBezier3D, CubicBezierChain3D, Polyhedron, QuadraticBezier3D},
     scad::{Scad, ScadColor, ScadOp, TextDirection, TextHalign, TextParams, TextValign},
     scad_tree_math::{
         approx_eq, dacos, dasin, datan, dcos, dsin, dtan, MersenneTwister, Mt4, Pt2, Pt2s, Pt3,
